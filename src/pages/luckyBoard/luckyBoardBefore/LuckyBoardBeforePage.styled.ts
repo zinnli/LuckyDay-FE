b7f0d3f8791d79812e75e759505e64c4188a287e
zinnli <102575747+zinnli@users.noreply.github.com>
2024-06-01 10:37:56 +0900

    style: 글자 잘림 이슈로 flex-start 적용(#159) (#161)

--- src/pages/luckyBoard/luckyBoardBefore/LuckyBoardBeforePage.styled.ts
+++ src/pages/luckyBoard/luckyBoardBefore/LuckyBoardBeforePage.styled.ts
@@ -5,7 +5,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   width: 100%;
   height: 80vh;
 `;
