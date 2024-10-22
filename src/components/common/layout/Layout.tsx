7026e652302726fac5ddf34074423c40d1c99cfc
yoorae <114637091+yooleng@users.noreply.github.com>
2024-10-23 00:39:10 +0900

    Fix/#219 tutorial, layout 누락 코드 반영 (#220)
    * fix: layout/tutorial branch 작업내용 종합 (#219)
    
    * fix: Layout에서 TutorialLayout 제거 (#219)

--- src/components/common/layout/Layout.tsx
+++ src/components/common/layout/Layout.tsx
@@ -1,20 +1,16 @@
 import * as S from "./Layout.styled";
 import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./header";
-import { TutorialLayout, useTutorial } from "components/tutorial";
 
 export default function Layout() {
   const { pathname } = useLocation();
-  const { isTutorialActive } = useTutorial();
+
+  const isHeaderVisible = !(pathname === "/404" || pathname === "/loading");
 
   return (
     <S.LayoutContainer>
       <S.Layout>
-        {!(pathname === "/loading" || pathname === "/404") && <Header />}
-        <Outlet />
-        {isTutorialActive && <TutorialLayout />}
-
-        {!(pathname === "/404" || pathname === "/loading") && (
+        {isHeaderVisible && (
           <S.HeaderContainer>
             <Header />
           </S.HeaderContainer>
